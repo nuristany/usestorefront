8a76f4eebc0f133d8210adf552195e056aa6f2dc
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-04-14 00:53:58 +0430

    commented src/component/CategoryPage.js

--- src/component/CategoryPage.js
+++ src/component/CategoryPage.js
@@ -1,92 +1,92 @@
 
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import { Link, useNavigate } from "react-router-dom";
 
-const CategoryPage = () => {
-  const [categories, setCategories] = useState([]);
-  const navigate = useNavigate(); // Initialize useNavigate hook
+// const CategoryPage = () => {
+//   const [categories, setCategories] = useState([]);
+//   const navigate = useNavigate(); // Initialize useNavigate hook
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const accessToken = localStorage.getItem("access");
-        const response = await axios.get(
-          "https://web-production-036f.up.railway.app/store/category/",
-          {
-            headers: {
-              Authorization: `JWT ${accessToken}`,
-            },
-          }
-        );
-        setCategories(response.data);
+//   useEffect(() => {
+//     const fetchCategories = async () => {
+//       try {
+//         const accessToken = localStorage.getItem("access");
+//         const response = await axios.get(
+//           "https://web-production-036f.up.railway.app/store/category/",
+//           {
+//             headers: {
+//               Authorization: `JWT ${accessToken}`,
+//             },
+//           }
+//         );
+//         setCategories(response.data);
 
-        // Fetch all items for each category and count them
-        const categoriesWithItemCount = await Promise.all(
-          response.data.map(async (category) => {
-            const itemsResponse = await axios.get(
-              `https://web-production-036f.up.railway.app/store/items/?category_id=${category.id}`,
-              {
-                headers: {
-                  Authorization: `JWT ${accessToken}`,
-                },
-              }
-            );
-            return { ...category, itemCount: itemsResponse.data.length };
-          })
-        );
+//         // Fetch all items for each category and count them
+//         const categoriesWithItemCount = await Promise.all(
+//           response.data.map(async (category) => {
+//             const itemsResponse = await axios.get(
+//               `https://web-production-036f.up.railway.app/store/items/?category_id=${category.id}`,
+//               {
+//                 headers: {
+//                   Authorization: `JWT ${accessToken}`,
+//                 },
+//               }
+//             );
+//             return { ...category, itemCount: itemsResponse.data.length };
+//           })
+//         );
 
-        setCategories(categoriesWithItemCount);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
+//         setCategories(categoriesWithItemCount);
+//       } catch (error) {
+//         console.error("Error fetching categories:", error);
+//       }
+//     };
 
-    fetchCategories();
-  }, []);
+//     fetchCategories();
+//   }, []);
 
-  const fetchItemsForCategory = async (categoryId) => {
-    try {
-      const accessToken = localStorage.getItem("access");
-      const response = await axios.get(
-        `https://web-production-036f.up.railway.app/store/items/?category_id=${categoryId}`,
-        {
-          headers: {
-            Authorization: `JWT ${accessToken}`,
-          },
-        }
-      );
+//   const fetchItemsForCategory = async (categoryId) => {
+//     try {
+//       const accessToken = localStorage.getItem("access");
+//       const response = await axios.get(
+//         `https://web-production-036f.up.railway.app/store/items/?category_id=${categoryId}`,
+//         {
+//           headers: {
+//             Authorization: `JWT ${accessToken}`,
+//           },
+//         }
+//       );
 
-      navigate(`/category-items/${categoryId}`);
-      console.log(response)
-    } catch (error) {
-      console.error(`Error fetching items for category ${categoryId}:`, error);
-    }
-  };
+//       navigate(`/category-items/${categoryId}`);
+//       console.log(response)
+//     } catch (error) {
+//       console.error(`Error fetching items for category ${categoryId}:`, error);
+//     }
+//   };
 
-  const handleCategoryClick = (categoryId) => {
-    fetchItemsForCategory(categoryId);
-  };
+//   const handleCategoryClick = (categoryId) => {
+//     fetchItemsForCategory(categoryId);
+//   };
 
-  return (
-    <div className="list-group">
-      <ul>
-      <h2 className="collection">Collections</h2>
-        {categories.map((category) => (
-          <Link key={category.id} onClick={() => handleCategoryClick(category.id)}>
-            <li>
-              {category.name} ({category.itemCount})
-            </li>
-          </Link>
-        ))}
-      </ul>
+//   return (
+//     <div className="list-group">
+//       <ul>
+//       <h2 className="collection">Collections</h2>
+//         {categories.map((category) => (
+//           <Link key={category.id} onClick={() => handleCategoryClick(category.id)}>
+//             <li>
+//               {category.name} ({category.itemCount})
+//             </li>
+//           </Link>
+//         ))}
+//       </ul>
      
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default CategoryPage;
+// export default CategoryPage;
 
 
 
